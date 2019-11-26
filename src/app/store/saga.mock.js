623411c59ef36b703fb06cbb9f779f5ea6e289eb
John LaTona <latona0204@gmail.com>
2019-11-26 16:34:32 -0500

    FE stuff

---
+++ src/app/store/saga.mock.js
@@ -0,0 +1,15 @@
+import { take, put, select} from 'redux-saga/effects';
+import * as mutations from './mutations';
+import uuid from 'uuid';
+
+export function* taskCreationSaga() {
+  while (true) {
+    const {groupID} = yield take(mutations.REQUEST_TASK_CREATION);
+    console.log("groupID")
+    const ownerID = 'U1';
+    const taskID = uuid();
+    yield put(mutations.createTask(taskID, groupID, ownerID));
+    console.log("got group ID", groupID);
+  }
+}
+
